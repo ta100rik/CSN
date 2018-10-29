de56873c6fe87e46b0f3c71c14b385536c8c9f3d
Zyad Osseyran <zyadosseyran@gmail.com>
2018-10-29 20:52:27 +0100

    Adding comments to it

--- client.py
+++ client.py
@@ -1,31 +1,47 @@
+# importing socket module
 import socket
-
+# importing RPI gpio plugin
 import RPi.GPIO as GPIO
+# importing time but not used
 import time
 
 HOST = '192.168.1.45'  # The server's hostname or IP address
 PORT = 65432        # The port used by the server
-
+# the type of socket connection
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# connecting to the socket
 s.connect((HOST, PORT))
 
 
-
+# looping throught the pins
 while True:
+	# defining pin1
 	pin1 = 14
+	# defining pin2
 	pin2 = 15
+	# gpio mode set to bcm
 	GPIO.setmode(GPIO.BCM)
+	# turn off warnings
 	GPIO.setwarnings(False)
+	# saying that pin2 will be out so that we turn on the led
 	GPIO.setup(pin1,GPIO.OUT)
+	# saying that pin2 will receive data
 	GPIO.setup(pin2,GPIO.IN)
+	# grabbing the data of pin2
 	sensor = GPIO.input(pin2)
+	# checking if the data is true so if the thing is broken (intruder)
 	if sensor == 1:
+		# turning on the led on pin1
 		GPIO.output(pin1,GPIO.HIGH)
+		# making data true that will be send later on
 		data = 'True'
+	# else if the chain is not broken turn of the led on pin 1
 	else:
+		# turning of the led
 	 	GPIO.output(pin1,GPIO.LOW)
+		# also data always can be handy
 	 	data = 'False'
+	# sending the data to the server
 	s.sendall(data)
+	# look if data is received
 	data = s.recv(1024)
-	# print('Received', repr(data))
-	# print(data)
--- server.py
+++ server.py
@@ -1,40 +1,56 @@
+# importing some modules to run the system
 import socket
 import sys
 import RPi.GPIO as GPIO
-
+import time
+# defining listen ip
 HOST = '192.168.1.45'  # Standard loopback interface address (localhost)
+# defining port to listen to
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+# defining type of socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# binding the host to the socket and the port so the socket will no what he/she is
 s.bind((HOST, PORT))
+# listening to the socket
 s.listen(20)
+# look if connection is accepted and is completed
 conn, addr = s.accept()
+# print that the things is connected
 print('Connected by', addr)
+# define pins where led is green or red
 green = 15
 red = 14
+# turning the mode of the GPIO pins to BCM
 GPIO.setmode(GPIO.BCM)
+# don't show warnings
 GPIO.setwarnings(False)
+# say that green en red ar both will send energy (bits) out
 GPIO.setup(green,GPIO.OUT)
-GPIO.setup(red,GPIO.OUT)	
+GPIO.setup(red,GPIO.OUT)
+# turn of all the lamps to be sure the that no 2 lamps will burn at the same time
 GPIO.output(green,GPIO.LOW)
 GPIO.output(red,GPIO.LOW)
-# GPIO.output(green,GPIO.HIGH)
+# looking for data loop
 while True:
+    # the received data will be in data
     data = conn.recv(10)
+    # checking if data is false or true
+    # when false green light will burn because system is on
     if data == 'False':
     	bol = True
     else:
     	bol = False
-    	# GPIO.output(green,GPIO.HIGH)
-    	# sleep(10)
-    	# GPIO.output(red,GPIO.LOW)
-		# GPIO.output(red,GPIO.HIGH)
-		# sleep(10)
-		# GPIO.output(red,GPIO.LOW)
+    # send something random back just to be sure that the package is received
     conn.sendall('hi')
+    # printing the status of the lamp
     print(bol)
+    # turning on the right light by checking the bolean above
     if bol:
     	GPIO.output(green,GPIO.HIGH)
     else:
     	GPIO.output(red,GPIO.HIGH)
+        # let the thing sleep because the red light need to be on for a while
+        time.sleep(10)
+    #turning of every lamp and check what the new status is
 	GPIO.output(red,GPIO.LOW)
 	GPIO.output(green,GPIO.LOW)
